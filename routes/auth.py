68742cb230eff02d08af8705639b032cf5535570
unknown <eliudjosuec@gmail.com>
2025-07-16 15:27:42 -0300

    fead:formularios con application/json

--- routes/auth.py
+++ routes/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 from models.models import User
@@ -7,8 +8,17 @@ from auth import hash_password, verify_password, create_access_token, get_user_b
 
 auth_router = APIRouter()
 
+# Modelos de entrada para los requests
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+class RegisterRequest(BaseModel):
+    username: str
+    password: str
+    
 @auth_router.post("/register")
-def register(form_data: OAuth2PasswordRequestForm = Depends()):
+def register(form_data: RegisterRequest):
     with Session(engine) as session:
         user_exist = get_user_by_username(form_data.username)
         if user_exist:
@@ -25,10 +35,11 @@ def register(form_data: OAuth2PasswordRequestForm = Depends()):
         return {"message": "Usuario creado"}
 
 @auth_router.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
+def login(form_data: LoginRequest):
     user = get_user_by_username(form_data.username)
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
 
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
+
